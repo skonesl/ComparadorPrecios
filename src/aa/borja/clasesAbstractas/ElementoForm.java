7a1c39def384b9e081ac77e8112ead4d3939b537
borja <borjasl2012@gmail.com>
2022-09-23 00:34:14 +0200

    Ejercicio Bebidas (Carpeta 6, fichero EJERCICIO OBJETOS Y UML EN JAVA, ejercicio 2) -> - Sin cambios
    Clase Udemy - Sección 19 Ejercicio Profesor
    
    - Iniciado Ejercicio (Hasta vídeo 194)

---
+++ src/aa/borja/clasesAbstractas/ElementoForm.java
@@ -0,0 +1,23 @@
+package aa.borja.clasesAbstractas;
+
+abstract public class ElementoForm {
+
+    protected String valor;
+    protected String nombre;
+
+    public ElementoForm() {
+    }
+
+    public ElementoForm(String nombre) {
+        this();
+        this.nombre = nombre;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    abstract public String dibujarHtml();
+
+
+}
